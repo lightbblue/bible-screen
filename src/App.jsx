07a9352f34370fad419b765d7fff743eedd3e150
lightbblue <jstachioni@gmail.com>
2025-02-03 04:01:28 -0300

    Add lucide icons, update app layout, and replace logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { BIBLE_IDS } from './constants'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // https://api.scripture.api.bible/v1/bibles?language=spa
+  const apiKey = "fac2182cab1f5f27c64d48b8ad9b7586"
+  
+  const [bible, setBible] = useState(BIBLE_IDS[0])
+  const [bibleId, setBibleId] = useState(bible.id)
+  const [bibleName, setBibleName] = useState(bible.nameLocal)
+  const [chapters, setChapters] = useState([])
+  
+  useEffect(() => {
+    setBibleId(bible.id)
+    setBibleName(bible.nameLocal)
+    fetch(`https://api.scripture.api.bible/v1/bibles/${bibleId}/books`, {
+      method: "GET",
+      headers: {
+        "api-key" : apiKey,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => setChapters(data.data.map(book => book.id)))
+      .catch(error => console.error("Error fetching data:", error));
+  }, [bible, bibleId]);
 
+  console.log(chapters)
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <img src="src\assets\img\bg.jpg"/>
+      <div className='wrapper'>
+        <header>
+          <div className='mid'>
+            <h2>Apocalipsis</h2>
+            <h2>1:10</h2>
+          </div>
+          <h2 className='bibleType'>{bibleName}</h2>
+        </header>
+        <main>
+          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo commodi molestiae eum fugit atque facilis qui quia unde vero voluptas praesentium, placeat autem voluptates quam asperiores saepe, omnis vitae doloremque!Lorem</h1>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+    
   )
 }
 
---
+++ src/constants.js
@@ -0,0 +1,450 @@
+export const BIBLE_IDS = [
+    {
+        "id": "592420522e16049f-01",
+        "dblId": "592420522e16049f",
+        "relatedDbl": null,
+        "name": "Reina Valera 1909",
+        "nameLocal": "Reina Valera 1909",
+        "abbreviation": "RVR09",
+        "abbreviationLocal": "RVR09",
+        "description": "Biblia Reina Valera 1909",
+        "descriptionLocal": "Biblia Reina Valera 1909",
+        "language": {
+            "id": "spa",
+            "name": "Spanish",
+            "nameLocal": "Español",
+            "script": "Latin",
+            "scriptDirection": "LTR"
+        },
+        "countries": [
+            {
+                "id": "ZZ",
+                "name": "Unspecific",
+                "nameLocal": "Unspecific"
+            }
+        ],
+        "type": "text",
+        "updatedAt": "2024-06-28T18:10:04.000Z",
+        "audioBibles": []
+    },
+    {
+        "id": "48acedcf8595c754-01",
+        "dblId": "48acedcf8595c754",
+        "relatedDbl": null,
+        "name": "Spanish Bible, Palabla de Dios para ti",
+        "nameLocal": "Palabla de Dios para ti",
+        "abbreviation": "spaPdDpt",
+        "abbreviationLocal": "PdDpt",
+        "description": "common",
+        "descriptionLocal": "común",
+        "language": {
+            "id": "spa",
+            "name": "Spanish",
+            "nameLocal": "Español",
+            "script": "Latin",
+            "scriptDirection": "LTR"
+        },
+        "countries": [
+            {
+                "id": "VE",
+                "name": "Venezuela, Bolivarian Republic of",
+                "nameLocal": "Venezuela, Bolivarian Republic of"
+            },
+            {
+                "id": "CO",
+                "name": "Colombia",
+                "nameLocal": "Colombia"
+            },
+            {
+                "id": "EC",
+                "name": "Ecuador",
+                "nameLocal": "Ecuador"
+            },
+            {
+                "id": "BO",
+                "name": "Bolivia, Plurinational State of",
+                "nameLocal": "Bolivia, Plurinational State of"
+            },
+            {
+                "id": "PE",
+                "name": "Peru",
+                "nameLocal": "Peru"
+            },
+            {
+                "id": "AR",
+                "name": "Argentina",
+                "nameLocal": "Argentina"
+            },
+            {
+                "id": "BR",
+                "name": "Brazil",
+                "nameLocal": "Brazil"
+            },
+            {
+                "id": "PY",
+                "name": "Paraguay",
+                "nameLocal": "Paraguay"
+            },
+            {
+                "id": "UY",
+                "name": "Uruguay",
+                "nameLocal": "Uruguay"
+            },
+            {
+                "id": "SV",
+                "name": "El Salvador",
+                "nameLocal": "El Salvador"
+            },
+            {
+                "id": "NI",
+                "name": "Nicaragua",
+                "nameLocal": "Nicaragua"
+            },
+            {
+                "id": "DO",
+                "name": "Dominican Republic",
+                "nameLocal": "Dominican Republic"
+            },
+            {
+                "id": "CU",
+                "name": "Cuba",
+                "nameLocal": "Cuba"
+            },
+            {
+                "id": "HN",
+                "name": "Honduras",
+                "nameLocal": "Honduras"
+            },
+            {
+                "id": "GT",
+                "name": "Guatemala",
+                "nameLocal": "Guatemala"
+            },
+            {
+                "id": "PA",
+                "name": "Panama",
+                "nameLocal": "Panama"
+            },
+            {
+                "id": "MX",
+                "name": "Mexico",
+                "nameLocal": "Mexico"
+            },
+            {
+                "id": "CL",
+                "name": "Chile",
+                "nameLocal": "Chile"
+            },
+            {
+                "id": "ES",
+                "name": "Spain",
+                "nameLocal": "Spain"
+            }
+        ],
+        "type": "text",
+        "updatedAt": "2024-06-28T17:11:54.000Z",
+        "audioBibles": []
+    },
+    {
+        "id": "48acedcf8595c754-02",
+        "dblId": "48acedcf8595c754",
+        "relatedDbl": null,
+        "name": "Spanish NT + PP, Palabla de Dios para ti",
+        "nameLocal": "Nuevo Contracto, Salmos y Proverbios, Palabla de Dios para ti",
+        "abbreviation": "spaPdDpt",
+        "abbreviationLocal": "PdDpt",
+        "description": "NTPP",
+        "descriptionLocal": "Nuevo Contracto y Salmos y Proverbios",
+        "language": {
+            "id": "spa",
+            "name": "Spanish",
+            "nameLocal": "Español",
+            "script": "Latin",
+            "scriptDirection": "LTR"
+        },
+        "countries": [
+            {
+                "id": "VE",
+                "name": "Venezuela, Bolivarian Republic of",
+                "nameLocal": "Venezuela, Bolivarian Republic of"
+            },
+            {
+                "id": "CO",
+                "name": "Colombia",
+                "nameLocal": "Colombia"
+            },
+            {
+                "id": "EC",
+                "name": "Ecuador",
+                "nameLocal": "Ecuador"
+            },
+            {
+                "id": "BO",
+                "name": "Bolivia, Plurinational State of",
+                "nameLocal": "Bolivia, Plurinational State of"
+            },
+            {
+                "id": "PE",
+                "name": "Peru",
+                "nameLocal": "Peru"
+            },
+            {
+                "id": "AR",
+                "name": "Argentina",
+                "nameLocal": "Argentina"
+            },
+            {
+                "id": "BR",
+                "name": "Brazil",
+                "nameLocal": "Brazil"
+            },
+            {
+                "id": "PY",
+                "name": "Paraguay",
+                "nameLocal": "Paraguay"
+            },
+            {
+                "id": "UY",
+                "name": "Uruguay",
+                "nameLocal": "Uruguay"
+            },
+            {
+                "id": "SV",
+                "name": "El Salvador",
+                "nameLocal": "El Salvador"
+            },
+            {
+                "id": "NI",
+                "name": "Nicaragua",
+                "nameLocal": "Nicaragua"
+            },
+            {
+                "id": "DO",
+                "name": "Dominican Republic",
+                "nameLocal": "Dominican Republic"
+            },
+            {
+                "id": "CU",
+                "name": "Cuba",
+                "nameLocal": "Cuba"
+            },
+            {
+                "id": "HN",
+                "name": "Honduras",
+                "nameLocal": "Honduras"
+            },
+            {
+                "id": "GT",
+                "name": "Guatemala",
+                "nameLocal": "Guatemala"
+            },
+            {
+                "id": "PA",
+                "name": "Panama",
+                "nameLocal": "Panama"
+            },
+            {
+                "id": "MX",
+                "name": "Mexico",
+                "nameLocal": "Mexico"
+            },
+            {
+                "id": "CL",
+                "name": "Chile",
+                "nameLocal": "Chile"
+            },
+            {
+                "id": "ES",
+                "name": "Spain",
+                "nameLocal": "Spain"
+            }
+        ],
+        "type": "text",
+        "updatedAt": "2024-06-28T17:12:16.000Z",
+        "audioBibles": []
+    },
+    {
+        "id": "b32b9d1b64b4ef29-01",
+        "dblId": "b32b9d1b64b4ef29",
+        "relatedDbl": null,
+        "name": "The Holy Bible in Simple Spanish",
+        "nameLocal": "La Biblia en Español Sencillo",
+        "abbreviation": "spabes",
+        "abbreviationLocal": "BES",
+        "description": "Common",
+        "descriptionLocal": "Común",
+        "language": {
+            "id": "spa",
+            "name": "Spanish",
+            "nameLocal": "Español",
+            "script": "Latin",
+            "scriptDirection": "LTR"
+        },
+        "countries": [
+            {
+                "id": "MX",
+                "name": "Mexico",
+                "nameLocal": "Mexico"
+            }
+        ],
+        "type": "text",
+        "updatedAt": "2024-06-29T00:05:39.000Z",
+        "audioBibles": []
+    },
+    {
+        "id": "482ddd53705278cc-01",
+        "dblId": "482ddd53705278cc",
+        "relatedDbl": null,
+        "name": "The New Testament in Spanish, Free Bible Version",
+        "nameLocal": "El Nuevo Testamento, Versión Biblia Libre",
+        "abbreviation": "VBL",
+        "abbreviationLocal": "VBL",
+        "description": "common",
+        "descriptionLocal": "Nuevo Testmento",
+        "language": {
+            "id": "spa",
+            "name": "Spanish",
+            "nameLocal": "Español",
+            "script": "Latin",
+            "scriptDirection": "LTR"
+        },
+        "countries": [
+            {
+                "id": "ES",
+                "name": "Spain",
+                "nameLocal": "Spain"
+            },
+            {
+                "id": "MX",
+                "name": "Mexico",
+                "nameLocal": "Mexico"
+            },
+            {
+                "id": "PY",
+                "name": "Paraguay",
+                "nameLocal": "Paraguay"
+            },
+            {
+                "id": "CO",
+                "name": "Colombia",
+                "nameLocal": "Colombia"
+            },
+            {
+                "id": "CL",
+                "name": "Chile",
+                "nameLocal": "Chile"
+            },
+            {
+                "id": "EC",
+                "name": "Ecuador",
+                "nameLocal": "Ecuador"
+            },
+            {
+                "id": "HN",
+                "name": "Honduras",
+                "nameLocal": "Honduras"
+            },
+            {
+                "id": "NI",
+                "name": "Nicaragua",
+                "nameLocal": "Nicaragua"
+            },
+            {
+                "id": "PE",
+                "name": "Peru",
+                "nameLocal": "Peru"
+            },
+            {
+                "id": "PA",
+                "name": "Panama",
+                "nameLocal": "Panama"
+            },
+            {
+                "id": "CU",
+                "name": "Cuba",
+                "nameLocal": "Cuba"
+            }
+        ],
+        "type": "text",
+        "updatedAt": "2024-06-28T17:10:21.000Z",
+        "audioBibles": []
+    },
+    {
+        "id": "482ddd53705278cc-02",
+        "dblId": "482ddd53705278cc",
+        "relatedDbl": null,
+        "name": "Versión Biblia Libre",
+        "nameLocal": "Versión Biblia Libre",
+        "abbreviation": "VBL",
+        "abbreviationLocal": "VBL",
+        "description": "Protestant",
+        "descriptionLocal": "Protestant",
+        "language": {
+            "id": "spa",
+            "name": "Spanish",
+            "nameLocal": "Español",
+            "script": "Latin",
+            "scriptDirection": "LTR"
+        },
+        "countries": [
+            {
+                "id": "ES",
+                "name": "Spain",
+                "nameLocal": "Spain"
+            },
+            {
+                "id": "MX",
+                "name": "Mexico",
+                "nameLocal": "Mexico"
+            },
+            {
+                "id": "PY",
+                "name": "Paraguay",
+                "nameLocal": "Paraguay"
+            },
+            {
+                "id": "CO",
+                "name": "Colombia",
+                "nameLocal": "Colombia"
+            },
+            {
+                "id": "CL",
+                "name": "Chile",
+                "nameLocal": "Chile"
+            },
+            {
+                "id": "EC",
+                "name": "Ecuador",
+                "nameLocal": "Ecuador"
+            },
+            {
+                "id": "HN",
+                "name": "Honduras",
+                "nameLocal": "Honduras"
+            },
+            {
+                "id": "NI",
+                "name": "Nicaragua",
+                "nameLocal": "Nicaragua"
+            },
+            {
+                "id": "PE",
+                "name": "Peru",
+                "nameLocal": "Peru"
+            },
+            {
+                "id": "PA",
+                "name": "Panama",
+                "nameLocal": "Panama"
+            },
+            {
+                "id": "CU",
+                "name": "Cuba",
+                "nameLocal": "Cuba"
+            }
+        ],
+        "type": "text",
+        "updatedAt": "2024-06-28T17:10:29.000Z",
+        "audioBibles": []
+    }
+];
